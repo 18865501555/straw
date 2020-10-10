e42d81278eca78f40777caab3cebb95fabbef7ea
18865501555 <66073067+18865501555@users.noreply.github.com>
2020-10-10 16:48:06 +0800

    update

--- straw-api/src/main/java/cn/tedu/straw/api/controller/UserController.java
+++ straw-api/src/main/java/cn/tedu/straw/api/controller/UserController.java
@@ -6,6 +6,7 @@ import cn.tedu.straw.api.ex.InsertException;
 import cn.tedu.straw.api.ex.InviteCodeException;
 import cn.tedu.straw.api.ex.PhoneDuplicateException;
 import cn.tedu.straw.api.service.IUserService;
+import cn.tedu.straw.commons.vo.R;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,18 +26,19 @@ public class UserController {
     IUserService userService;
     // http://localhost:8080/api/v1/users/student/register?phone=13100131001&password=1234&inviteCode=JSD2004-666666
     @RequestMapping("/student/register")
-    public String regStudent(StudentRegisterDTO studentRegisterDTO) {
+    public R regStudent(StudentRegisterDTO studentRegisterDTO) {
+        R r = new R();
         try {
             userService.regStudent(studentRegisterDTO);
-            return "注册成功!";
+            return r.setMessage("注册成功!");
         } catch (InviteCodeException e) {
-            return "注册失败!邀请码错误!";
+            return r.setMessage("注册失败!邀请码错误!");
         } catch (PhoneDuplicateException e) {
-            return "注册失败!手机号码已经被占用!";
+            return r.setMessage("注册失败!手机号码已经被占用!");
         } catch (InsertException e) {
-            return "注册失败!服务器忙!请稍后再次尝试!";
+            return r.setMessage("注册失败!服务器忙!请稍后再次尝试!");
         } catch (Throwable e) {
-            return "注册失败!出现预期以外的异常!" + e.getClass().getName() + "," + e.getMessage();
+            return r.setMessage("注册失败!出现预期以外的异常!" + e.getClass().getName() + "," + e.getMessage());
         }
     }
 }
---
+++ straw-commons/src/main/java/cn/tedu/straw/commons/vo/R.java
@@ -0,0 +1,11 @@
+package cn.tedu.straw.commons.vo;
+
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+@Data
+@Accessors(chain = true)
+public class R {
+
+    private String message;
+}
