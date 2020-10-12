47006ca1bb3523de8b17d69527b94a62150f06b9
18865501555 <66073067+18865501555@users.noreply.github.com>
2020-10-12 13:53:30 +0800

    update

--- straw-api/src/test/java/cn/tedu/straw/api/StrawApiApplicationTests.java
+++ straw-api/src/test/java/cn/tedu/straw/api/StrawApiApplicationTests.java
@@ -1,5 +1,6 @@
 package cn.tedu.straw.api;
 
+import org.apache.commons.codec.digest.DigestUtils;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -20,5 +21,21 @@ class StrawApiApplicationTests {
     @Test
     void getConnection() throws SQLException {
         System.err.println(dataSource.getConnection());
+    }
+
+    @Test
+    void md5Tests() {
+        String password = "1234";
+        String encodePassword = DigestUtils.md5Hex(password);
+        System.err.println("[md5] encode password=" + encodePassword);
+
+        encodePassword = DigestUtils.sha256Hex(password);
+        System.err.println("[sha-256] encode password=" + encodePassword);
+
+        encodePassword = DigestUtils.sha384Hex(password);
+        System.err.println("[sha-384] encode password=" + encodePassword);
+
+        encodePassword = DigestUtils.sha512Hex(password);
+        System.err.println("[sha-512] encode password=" + encodePassword);
     }
 }
