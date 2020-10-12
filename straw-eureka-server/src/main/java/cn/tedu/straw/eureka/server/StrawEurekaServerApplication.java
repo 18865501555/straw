d3eeedf05aee870981e511191d85419b4fb1d816
18865501555 <66073067+18865501555@users.noreply.github.com>
2020-10-12 17:31:36 +0800

    update

---
+++ straw-eureka-server/src/main/java/cn/tedu/straw/eureka/server/StrawEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package cn.tedu.straw.eureka.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrawEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StrawEurekaServerApplication.class, args);
+	}
+
+}
---
+++ straw-eureka-server/src/test/java/cn/tedu/straw/eureka/server/StrawEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.tedu.straw.eureka.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrawEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
