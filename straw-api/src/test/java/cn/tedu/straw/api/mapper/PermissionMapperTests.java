2d81bc920108b3303bd57d739d3a23aa8262cdf4
18865501555 <66073067+18865501555@users.noreply.github.com>
2020-10-13 15:57:06 +0800

    update

---
+++ straw-api/src/test/java/cn/tedu/straw/api/mapper/PermissionMapperTests.java
@@ -0,0 +1,25 @@
+package cn.tedu.straw.api.mapper;
+
+import cn.tedu.straw.commons.model.Permission;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+@SpringBootTest
+@Slf4j
+public class PermissionMapperTests {
+    @Autowired
+    PermissionMapper permissionMapper;
+    @Test
+    void findByUsername(){
+        String username = "13800138014";
+        List<Permission> permissions = permissionMapper.findByUsername(username);
+        log.debug("permission list size >>> {}", permissions.size());
+        for (Permission permission : permissions) {
+            log.debug(">>> {}",permission);
+        }
+    }
+}
