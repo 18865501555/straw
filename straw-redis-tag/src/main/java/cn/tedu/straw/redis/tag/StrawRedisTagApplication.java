86921ae43cdb862a5da736d4578cebd697287879
18865501555 <66073067+18865501555@users.noreply.github.com>
2020-10-15 09:46:48 +0800

    update

---
+++ straw-redis-tag/src/main/java/cn/tedu/straw/redis/tag/StrawRedisTagApplication.java
@@ -0,0 +1,13 @@
+package cn.tedu.straw.redis.tag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrawRedisTagApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StrawRedisTagApplication.class, args);
+	}
+
+}
