384c4942af0284b0e8c93474b450d1a7280a0e5a
18865501555 <66073067+18865501555@users.noreply.github.com>
2020-10-16 11:32:06 +0800

    update

--- straw-gateway/src/main/resources/static/js/question/create.js
+++ straw-gateway/src/main/resources/static/js/question/create.js
@@ -11,4 +11,27 @@ let createQuestionApp = new Vue({
         ],
         selectedTagIds: []
     },
+    methods: {
+        loadTags: function () {
+            $.ajax({
+                url: '/redis-tag/v1/tags',
+                type: 'get',
+                dataType: 'json',
+                success: function (json) {
+                    let list = json.data;
+                    let tags = [];
+                    for (let i = 0; i < list.length; i++) {
+                        let op = {};
+                        op.label = list[i].name;
+                        op.value = list[i].id;
+                        tags[i] = op;
+                    }
+                    createQuestionApp.tags = tags;
+                }
+            });
+        }
+    },
+    created: function () {
+        this.loadTags();
+    }
 });
