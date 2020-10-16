c23d574c26a00b342000a51b6951e0814ceca5c0
18865501555 <66073067+18865501555@users.noreply.github.com>
2020-10-16 11:20:28 +0800

    update

--- straw-gateway/src/main/resources/static/js/question/create.js
+++ straw-gateway/src/main/resources/static/js/question/create.js
@@ -1,13 +1,14 @@
 Vue.component('v-select', VueSelect.VueSelect);
 let createQuestionApp = new Vue({
-    el:'#createQuestionApp',
-    data:{
-        tags:[
-            {label:'Eureka', value: 1},
-            {label:'Zuul', value: 2},
-            {label:'Redis', value: 3},
-            {label:'Vue', value: 4},
-            {label:'Spring Security', value: 5}
-        ]
+    el: '#createQuestionApp',
+    data: {
+        tags: [
+            {label: 'Eureka', value: 1},
+            {label: 'Zuul', value: 2},
+            {label: 'Redis', value: 3},
+            {label: 'Vue', value: 4},
+            {label: 'Spring Security', value: 5}
+        ],
+        selectedTagIds: []
     },
 });
