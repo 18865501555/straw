d5006ddb72da8eedded4bf8dc5213e9dd7552e6c
18865501555 <66073067+18865501555@users.noreply.github.com>
2020-10-16 10:10:16 +0800

    update

--- straw-gateway/src/main/resources/static/js/commons/nav_tags.js
+++ straw-gateway/src/main/resources/static/js/commons/nav_tags.js
@@ -11,7 +11,15 @@ let tagsApp = new Vue({
     },
     methods:{
         loadTags: function () {
-            alert("准备加载标签列表数据......");
+            // alert("准备加载标签列表数据......");
+            $.ajax({
+                url: '/redis-tag/v1/tags',
+                type: 'get',
+                dataType: 'json',//json,text,xml >>> 取值取决于content-type
+                success: function (json) {  // 回调函数
+                    tagsApp.tags = json.data;
+                }
+            });
         }
     },
     created: function () {
